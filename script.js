e2ef68bb9afc54795a84f4c560561071dda20064
Wiktor <113196554+Wikshello@users.noreply.github.com>
2023-10-24 17:41:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+function z1() {
+   window.open("z1_Ejsm.html", "okienko_z1","toolbar=no, directories=no, menubar=no, height=75, width=75, top=180, left=200");
+}
+
+function z2() {
+   window.open("z2_Ejsm.html", "okienko_z2","toolbar=no, directories=no, menubar=no, height=100, width=450, top=350, left=200");
+}
+
+function z3() {
+   window.open("z3_Ejsm.html", "okienko_z3","toolbar=no, directories=no, menubar=no, height=200, width=160, top=200, left=200");
+}
+ 
+function z4() {
+   window.open("z4_Ejsm.html", "okienko_z4",  "toolbar=no, directories=no, menubar=no, height=1000, width=1000, top=200, left=400");
+}
+ 
+function z5() {
+   window.open("z5_Ejsm.html", "okienko_z5","toolbar=no, directories=no, menubar=no, height=350, width=700, top=350, left=600");
+}
+
+function zal3_5() {
+   window.open("zal3_5_Ejsm.html", "okienko_zal3_5","toolbar=no, directories=no, menubar=no, height=900, width=1100, top=100, left=400");
+}
+
+function z6() {
+   window.open("z6_Ejsm.html", "okienko_z6","toolbar=no, directories=no, menubar=no, height=700, width=1000, top=350, left=600");
+}
+
+function z7() {
+   window.open("z7_Ejsm.html", "okienko_z7","toolbar=no, directories=no, menubar=no, height=350, width=700, top=350, left=600");
+}
+
+function z8() {
+   window.open("z8_Ejsm.html", "okienko_z8","toolbar=no, directories=no, menubar=no, height=350, width=700, top=350, left=600");
+}
+
+function z9() {
+   window.open("z9_Ejsm.html", "okienko_z9","toolbar=no, directories=no, menubar=no, height=350, width=700, top=350, left=600");
+}
+
+function z10() {
+   window.open("z10_Ejsm.html", "okienko_z10","toolbar=no, directories=no, menubar=no, height=350, width=700, top=350, left=600");
+}
+
+function z11() {
+   window.open("z11_Ejsm.html", "okienko_z11","toolbar=no, directories=no, menubar=no, height=350, width=700, top=350, left=600");
+}
+
+function z12() {
+   window.open("z12_Ejsm.html", "okienko_z12","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z13() {
+   window.open("z13_Ejsm.html", "okienko_z13","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z14() {
+   window.open("z14_Ejsm.html", "okienko_z14","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z15() {
+   window.open("z15_Ejsm.html", "okienko_z15","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z11_kod() {
+   window.open("z11_Ejsm_kod.html", "okienko_z11_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z12_kod() {
+   window.open("z12_Ejsm_kod.html", "okienko_z12_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z13_kod() {
+   window.open("z13_Ejsm_kod.html", "okienko_z13_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z14_kod() {
+   window.open("z14_Ejsm_kod.html", "okienko_z14_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z15_kod() {
+   window.open("z15_Ejsm_kod.html", "okienko_z15_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
