b399abbe10a592d645f12cc124bce6a89c422afc
Wiktor <113196554+Wikshello@users.noreply.github.com>
2024-01-11 19:16:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -67,6 +67,29 @@ function z16() {
 }
 
 
+function z28() {
+   window.open("z28_Ejsm.html", "okienko_z28","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+}
+
+function z29() {
+   window.open("z29_Ejsm.html", "okienko_z29","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+}
+
+function z30() {
+   window.open("z30_Ejsm.html", "okienko_z30","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z31() {
+   window.open("z31_Ejsm.html", "okienko_z31","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+function z32() {
+   window.open("z32_Ejsm.html", "okienko_z32","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+}
+
+
+
+
 
 
 function z11_kod() {
@@ -91,4 +114,17 @@ function z15_kod() {
 
 function z16_kod() {
    window.open("z16_Ejsm_kod.html", "okienko_z16_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+
+function z30_kod() {
+   window.open("z30_Ejsm_kod.html", "okienko_z30_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z31_kod() {
+   window.open("z31_Ejsm_kod.html", "okienko_z31_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+}
+
+function z32_kod() {
+   window.open("z32_Ejsm_kod.html", "okienko_z32_kod","toolbar=no, directories=no, menubar=no, height=720, width=800, top=350, left=600");
 }
